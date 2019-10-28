63e39d3d04aa99ae6c494a04f275aebf97a33c79
李翔 <lx70770@gmail.com>
2019-10-28 19:42:19 +0800

    :tada: init commit

---
+++ jest.config.js
@@ -0,0 +1,63 @@
+module.exports = {
+  // 设置根目录
+  roots: ["<rootDir>/src"],
+
+  // 定义分析的测试文件  加叹号为忽略这类文件
+  collectCoverageFrom: ["src/**/*.{js,jsx,ts,tsx}", "!src/**/*.d.ts"],
+
+  // 运行测试之前 加的垫片 解决兼容问题
+  setupFiles: ["react-app-polyfill/jsdom"],
+
+  // 做一些额外的文件配置
+  setupFilesAfterEnv: ["./node_modules/jest-enzyme/lib/index.js"],
+
+  // 定义测试文件夹和文件
+  testMatch: [
+    "<rootDir>/src/**/__tests__/**/*.{js,jsx,ts,tsx}",
+    "<rootDir>/src/**/*.{spec,test}.{js,jsx,ts,tsx}"
+  ],
+
+  // 模拟js DOM环境
+  testEnvironment: "jest-environment-jsdom-fourteen",
+
+  // 语法解析操作
+  transform: {
+    "^.+\\.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/babel-jest", // 转换ES6代码
+    "^.+\\.css$": "<rootDir>/config/jest/cssTransform.js", // 转换CSS代码
+    "^(?!.*\\.(js|jsx|ts|tsx|css|json)$)":
+      "<rootDir>/config/jest/fileTransform.js" // 转换其他文件
+  },
+  transformIgnorePatterns: [
+    "[/\\\\]node_modules[/\\\\].+\\.(js|jsx|ts|tsx)$",
+    "^.+\\.module\\.(css|sass|scss)$" // 忽略一些文件转换（node_modules）
+  ],
+
+  // 默认引入依赖的文件夹
+  modulePaths: [],
+
+  // 测试样式名  转换样式名  变换原始的css名为普通对象
+  moduleNameMapper: {
+    "^react-native$": "react-native-web",
+    "^.+\\.module\\.(css|sass|scss)$": "identity-obj-proxy"
+  },
+
+  // 文件缺省后缀自动寻找
+  moduleFileExtensions: [
+    "web.js",
+    "js",
+    "web.ts",
+    "ts",
+    "web.tsx",
+    "tsx",
+    "json",
+    "web.jsx",
+    "jsx",
+    "node"
+  ],
+
+  // 测试插件
+  watchPlugins: [
+    "jest-watch-typeahead/filename",
+    "jest-watch-typeahead/testname"
+  ]
+};
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import Enzyme, { shallow, mount } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import App from "./App";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+Enzyme.configure({ adapter: new Adapter() });
+
+describe("测试enzyme的shallow", () => {
+  const wrapper = shallow(<App />);
+  const container = wrapper.find("[data-jest='container']");
+
+  it("使用Enzyme测试DOM元素", () => {
+    console.log(wrapper.debug());
+    expect(container.length).toBe(1);
+    expect(container.prop("name")).toBe("lixiang");
+  });
+
+  it("使用jest-enzyme方便测试DOM", () => {
+    expect(container).toExist();
+    expect(container).toHaveProp("name", "lixiang");
+  });
+});
+
+describe("测试enzyme的mount", () => {
+  const wrapper = mount(<App />);
+
+  // 某些场景是不能修改DOM元素的 可以用快照测试
+  it("使用Enzyme测试快照", () => {
+    expect(wrapper).toMatchSnapshot();
+  });
 });
